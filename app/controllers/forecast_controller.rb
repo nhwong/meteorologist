586fca84348a1089d3e4fb6b3aa0c974569620e7
Nate Wong <nathaniel.h.wong@gmail.com>
2016-10-18 19:38:23 -0500

    Completed coords_to_weather

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,19 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    dark_sky_url = "https://api.darksky.net/forecast/2ce3841872e63a6c9efcad040662259a/"
+    full_url = dark_sky_url + @lat.to_s + "," + @lng.to_s
+    dark_sky_hash = JSON.parse(open(full_url).read)
 
+    @current_temperature = dark_sky_hash["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary =  dark_sky_hash["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = dark_sky_hash["minutely"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = dark_sky_hash["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = dark_sky_hash["daily"]["summary"]
 
     render("forecast/coords_to_weather.html.erb")
   end
